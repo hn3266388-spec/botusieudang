d2ac7feffb6d437c6ea198e50a428735ff18201f
NgocKyDev <ngockydinh05@gmail.com>
2026-07-08 21:20:01 +0700

    Fix axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ranch-expediter-excursion.ngrok-free.dev/',  
+  baseURL: 'https://ranch-expediter-excursion.ngrok-free.dev',  
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 });
